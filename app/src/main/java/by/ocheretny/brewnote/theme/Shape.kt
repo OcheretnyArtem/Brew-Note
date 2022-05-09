34bad8c85c512d07cd4080f3775fbd9ce886d24e
OcheretnyArtem <ocheretnyartem@gmail.com>
2022-05-09 04:12:11 +0300

    Added BrewNoteApp class

--- app/src/main/java/by/ocheretny/brewnote/theme/Shape.kt
+++ app/src/main/java/by/ocheretny/brewnote/theme/Shape.kt
@@ -1,4 +1,4 @@
-package by.ocheretny.brewnote.ui.theme
+package by.ocheretny.brewnote.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
--- app/src/main/java/by/ocheretny/brewnote/theme/Type.kt
+++ app/src/main/java/by/ocheretny/brewnote/theme/Type.kt
@@ -1,4 +1,4 @@
-package by.ocheretny.brewnote.ui.theme
+package by.ocheretny.brewnote.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
