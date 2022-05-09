61afe42050e4257cf26ba71aa6bd0acf89f38ac8
OcheretnyArtem <ocheretnyartem@gmail.com>
2022-05-10 02:09:02 +0300

    Added database

---
+++ data/src/main/java/by/ocheretny/data/database/entity/InfusionEntity.kt
@@ -0,0 +1,16 @@
+package by.ocheretny.data.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "infusion_table")
+internal data class InfusionEntity(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+    val profileId: String,
+    val startSecond: Int,
+    val endSecond: Int,
+    val waitingTime: Int,
+    val countOfWater: Int,
+    val isMixed: Boolean = false
+)
