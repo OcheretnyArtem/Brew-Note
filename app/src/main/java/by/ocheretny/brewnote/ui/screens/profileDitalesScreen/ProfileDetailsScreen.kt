0a46e43585ab52b5e4992c901df3dd14fa75f45f
OcheretnyArtem <ocheretnyartem@gmail.com>
2022-07-24 23:38:42 +0300

    Added Details Screen

---
+++ app/src/main/java/by/ocheretny/brewnote/ui/screens/profileDitalesScreen/ProfileDetailsScreen.kt
@@ -0,0 +1,13 @@
+package by.ocheretny.brewnote.ui.screens.profileDitalesScreen
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+@Composable
+@Destination
+fun ProfileDetailsScreen(
+    navigator: DestinationsNavigator
+) {
+
+}
---
+++ app/src/main/java/by/ocheretny/brewnote/ui/screens/profileDitalesScreen/viewModel/ProfileDetailsActionState.kt
@@ -0,0 +1,6 @@
+package by.ocheretny.brewnote.ui.screens.profileDitalesScreen.viewModel
+
+import by.ocheretny.brewnote.base.viewModel.ActionState
+
+class ProfileDetailsActionState : ActionState {
+}
