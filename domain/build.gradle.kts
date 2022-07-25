d8180cf96e84aac596c85c6631c0653e67c22077
OcheretnyArtem <ocheretnyartem@gmail.com>
2022-07-25 22:20:15 +0300

    Migrated on gradle.kts with buildSrc

---
+++ domain/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
+}
+
+android {
+    compileSdk = BrewNoteConfiguration.compileSdkVersion
+
+    defaultConfig {
+        minSdk = BrewNoteConfiguration.minSdkVersion
+        targetSdk = BrewNoteConfiguration.targetSdkVersion
+
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles.add(File("consumer-rules.pro"))
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(File("proguard-android-optimize.txt"), File("proguard-rules.pro"))
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    //Coroutines
+    BrewNoteDependencies.coroutines(this)
+}
