de490357a41ff044bca5827f08713c245f8b5fbc
OcheretnyArtem <ocheretnyartem@gmail.com>
2022-07-26 11:18:44 +0300

    Added DispatcherProvider

--- data/src/main/kotlin/by/data/repositories/DatabaseRepositoryImpl.kt
+++ data/src/main/kotlin/by/data/repositories/DatabaseRepositoryImpl.kt
@@ -7,11 +7,11 @@ import by.data.database.entity.ProfileEntity
 import by.data.database.relations.ProfileWithCoffeeAndInfusions
 import by.data.parsres.Mapper
 import by.data.parsres.Parser
+import by.domain.coroutines.DispatcherProvider
 import by.domain.entity.Coffee
 import by.domain.entity.Infusion
 import by.domain.entity.Profile
 import by.domain.repositories.DatabaseRepository
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
@@ -26,10 +26,10 @@ internal class DatabaseRepositoryImpl @Inject constructor(
     private val mapperProfileDomainToEntity: Mapper<Profile, ProfileEntity>,
     private val parserCoffeeDomainEntity: Parser<Coffee, CoffeeEntity>,
     private val parserInfusionDomainEntity: Parser<Infusion, InfusionEntity>,
-    private val dispatchers: CoroutineDispatcher,
+    private val dispatchers: DispatcherProvider,
 ) : DatabaseRepository {
 
-    override suspend fun getProfileById(id: Int): Profile = withContext(dispatchers) {
+    override suspend fun getProfileById(id: Int): Profile = withContext(dispatchers.io) {
         mapperProfileEntityToDomain.map(dao.getProfileById(id))
     }
 
@@ -37,37 +37,37 @@ internal class DatabaseRepositoryImpl @Inject constructor(
         return@map it.map { profile ->
             mapperProfileEntityToDomain.map(profile)
         }
-    }.flowOn(dispatchers)
+    }.flowOn(dispatchers.io)
 
     override suspend fun getAllCoffee(): Flow<List<Coffee>> = dao.getAllCoffee().map {
         return@map it.map { coffee ->
             parserCoffeeDomainEntity.pars(coffee)
         }
-    }.flowOn(dispatchers)
+    }.flowOn(dispatchers.io)
 
-    override suspend fun insert(profile: Profile) = withContext(dispatchers) {
+    override suspend fun insert(profile: Profile) = withContext(dispatchers.io) {
         dao.insert(mapperProfileDomainToEntity.map(profile))
     }
 
-    override suspend fun insert(coffee: Coffee) = withContext(dispatchers) {
+    override suspend fun insert(coffee: Coffee) = withContext(dispatchers.io) {
         dao.insert(parserCoffeeDomainEntity.unPars(coffee))
     }
 
-    override suspend fun insert(infusion: Infusion) = withContext(dispatchers) {
+    override suspend fun insert(infusion: Infusion) = withContext(dispatchers.io) {
         dao.insert(parserInfusionDomainEntity.unPars(infusion))
     }
 
-    override suspend fun insert(infusions: List<Infusion>) = withContext(dispatchers) {
+    override suspend fun insert(infusions: List<Infusion>) = withContext(dispatchers.io) {
         infusions.forEach { infusion ->
             dao.insert(parserInfusionDomainEntity.unPars(infusion))
         }
     }
 
-    override suspend fun delete(coffee: Coffee) = withContext(dispatchers) {
+    override suspend fun delete(coffee: Coffee) = withContext(dispatchers.io) {
         dao.delete(dao.getCoffeeWithProfilesByCoffeeId(coffee.id!!))
     }
 
-    override suspend fun delete(profile: Profile) = withContext(dispatchers) {
+    override suspend fun delete(profile: Profile) = withContext(dispatchers.io) {
         dao.delete(dao.getProfileById(profile.coffee.id!!))
     }
 }
