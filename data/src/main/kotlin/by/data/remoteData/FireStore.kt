58a5706e997b9efd341a0aac7361d3cac055619d
OcheretnyArtem <ocheretnyartem@gmail.com>
2022-07-29 06:15:48 +0300

    Added constants for FireStore

--- data/src/main/kotlin/by/data/remoteData/FireStore.kt
+++ data/src/main/kotlin/by/data/remoteData/FireStore.kt
@@ -6,30 +6,22 @@ import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+private const val USERS = "users"
+private const val GROUPS = "groups"
+private const val PROFILES = "profiles"
+private const val COFFEE = "coffee"
+private const val INFUSIONS = "infusions"
+
 internal class FireStore @Inject constructor(
     private val fireStore: FirebaseFirestore,
     private val dispatchers: DispatcherProvider,
 ) : RemoteService {
 
-//    override suspend fun getUsersByName(s: String): Flow<List<User>> {
-//        val list: MutableList<User> = ArrayList()
-//        fireStore.collection("users").li
-//            .whereEqualTo("name", s)
-//            .get()
-//            .addOnSuccessListener { task ->
-//                for (document in task) {
-//                    list.add(document.toObject())
-//                }
-//            }.await()
-//        return flowOf(list)
-//    }
-
     override suspend fun getUsersByName(s: String): Flow<List<User>> = observeFromFireStore(
-        dbRef = fireStore.collection("users"),
+        dbRef = fireStore.collection(USERS),
         coroutineContext = dispatchers.io,
         keySearchQuery = "name",
         valueSearchQuery = s
     )
-
 
 }
