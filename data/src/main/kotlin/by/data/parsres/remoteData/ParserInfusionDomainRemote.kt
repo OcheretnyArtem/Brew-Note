b84ca70a05b68f238c0f6257467695e6c4deaec8
OcheretnyArtem <ocheretnyartem@gmail.com>
2022-07-29 19:01:30 +0300

    Added ParserInfusionDomainRemote

---
+++ data/src/main/kotlin/by/data/parsres/remoteData/ParserInfusionDomainRemote.kt
@@ -0,0 +1,28 @@
+package by.data.parsres.remoteData
+
+import by.data.parsres.Parser
+import by.data.remote.entities.InfusionRemote
+import by.domain.entities.Infusion
+
+internal class ParserInfusionDomainRemote : Parser<Infusion, InfusionRemote> {
+
+    override fun pars(from: InfusionRemote): Infusion = Infusion(
+        id = from.id,
+        profileId = from.profileId ?: 0,
+        startSecond = from.startSecond ?: 0,
+        endSecond = from.endSecond ?: 0,
+        waitingTime = from.waitingTime ?: 0,
+        countOfWater = from.countOfWater ?: 0,
+        isMixed = from.isMixed ?: false
+    )
+
+    override fun unPars(from: Infusion): InfusionRemote = InfusionRemote(
+        id = from.id,
+        profileId = from.profileId,
+        startSecond = from.startSecond,
+        endSecond = from.endSecond,
+        waitingTime = from.waitingTime,
+        countOfWater = from.countOfWater,
+        isMixed = from.isMixed
+    )
+}
