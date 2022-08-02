0cb023f51cbd945e49e07cbb3a31d8627ae72c48
OcheretnyArtem <ocheretnyartem@gmail.com>
2022-08-02 06:11:17 +0300

    Added DataStoreModule

--- data/src/main/kotlin/by/data/localData/dataStores/userID/UserIdDaoImpl.kt
+++ data/src/main/kotlin/by/data/localData/dataStores/userID/UserIdDaoImpl.kt
@@ -6,6 +6,7 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -14,7 +15,7 @@ private const val STORE_NAME = "USER_ID_STORE_NAME"
 private const val USER_ID = "ID"
 
 class UserIdDaoImpl @Inject constructor(
-    private val context: Context
+    @ApplicationContext private val context: Context
 ) : UserIdDao {
 
     private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = STORE_NAME)
